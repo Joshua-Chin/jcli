c6db6521cd9d8d74c0f796a19c013534a0831c1f
Zane Sterling <sterling.zane@gmail.com>
2014-05-31 20:17:47 -0400

    Fix typos

---
+++ jcli.py
@@ -0,0 +1,53 @@
+import tokenizer
+import parser_
+import compiler
+import assembler
+import executor
+from jcli_builtins import jcli_builtins
+from datatypes import sym
+
+def jcli_exec(srcs, steps, step, spr, callbacks=None, builtins=None):
+    if builtins is None:
+        builtins = jcli_builtins
+    executors = []
+    for index, src in enumerate(srcs):
+        globals_ = dict(builtins)
+        globals_.update(gen_callbacks(index, callbacks))
+        executors.append(eval_lisp(src))
+    out = [None]*len(srcs)
+    for step_count in range(steps*spr):
+        if not step_count % spr:
+            step()
+        for index, executor in enumerate(executors):
+            if executor is None:
+                continue
+            try:
+                next(executor)
+                out[index] = "Out of Time"
+            except StopIteration:
+                executors[index] = None
+            except Exception as e:
+                out[index] = type(e).__name__ + ": " + e.message
+                executors[index] = None
+    return out
+
+def gen_callbacks(index, callbacks):
+        return {sym(s):lambda *args, **kwargs: f(index, *args, **kwargs)
+                for s,f in callbacks.items()}
+
+def eval_lisp(src, builtins=None):
+    if builtins is None:
+        builtins = dict(jcli_builtins)
+    return execute_lisp(compile_lisp(src), builtins)
+
+def compile_lisp(src):
+    tokens = tokenizer.tokenize(src)
+    ast = parser_.parse(tokens)
+    asm = compiler.compile_(ast)
+    bytecode = assembler.assemble(asm)
+    return bytecode
+
+def execute_lisp(bytecode, builtins=None):
+    if builtins is None:
+        builtins = dict(jcli_builtins)
+    return executor.execute_bytecodes(bytecode, builtins)
