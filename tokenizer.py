5a7e6d03b372668f164704027821186d1bdc4350
Joshua Chin <joshuarchin@gmail.com>
2014-06-02 12:28:41 -0400

    improved tokenizer

---
+++ tokenizer.py
@@ -0,0 +1,60 @@
+import collections
+import enum
+import re
+
+from datatypes import sym
+
+class Token(enum.Enum):
+    left_paren = '('
+    right_paren = ')'
+    quote = '\''
+    quasiquote = '`'
+    unquote = ','
+
+def tokenize_str(string):
+    return string[1:-1].encode('utf8').decode('unicode-escape')
+    
+token_types = collections.OrderedDict([
+    (float, r'(-|\+)?((\d+\.\d*)|(\d*\.\d+))'),
+    (int, r'(-|\+)?\d+'),
+    (tokenize_str, r'"(\\"|[^"])*"'),
+    (lambda x: x=='#t', r'#t|#f'),
+    (Token, r'[\(\)\',`]'),
+    (None, r'(\s+|;.*)'),
+    (sym, r'[^"\'`,\.#;\s\(\)\[\]\{\}]+'),])
+
+def compile_(tokens):
+    for token, regex in list(tokens.items()):
+        tokens[token] = re.compile(regex)
+compile_(token_types)
+
+def tokenize(string, tokens=None):
+    if tokens is None:
+        tokens = token_types
+    out = []
+    index = 0
+    while index < len(string):
+        head = string[index:]
+        for token, regex in list(tokens.items()):
+            match = regex.match(head)
+            if match:
+                group = match.group()
+                index += len(group)
+                if token is not None:
+                    out.append(token(group))
+                else:
+                    out.append(None)
+                break
+        else:
+            raise AssertionError(head)
+    for a, b in zip(out, out[1:]):
+        if isinstance(a, Token) or a is None:
+            continue
+        if isinstance(b, Token) or b is None:
+            continue
+        raise AssertionError(b)
+    return filter(None, out)
+
+if __name__ == '__main__':
+    while True:
+        print((tokenize(raw_input("tokenize> "))))
