1526de00b1967039c598ddbb631ddf65d7e5e1e8
Joshua Chin <joshuarchin@gmail.com>
2014-07-27 12:41:07 -0400

    Allow for pure lisp includes
    All higher order functions must be coded in pure lisp in the
    jclli_includes.jcli file.

--- jcli.py
+++ jcli.py
@@ -11,9 +11,31 @@ import executor
 from builtins_ import jcli_builtins
 from datatypes import sym
 
-def jcli_exec(srcs, steps, step, spr, callbacks=None, builtins=None, debug=False):
+def jcli_exec(srcs, steps, step, spr, callbacks=None, builtins=None, includes=None,debug=False):
+    """Executes a list of sources in parallel
+    srcs  [str]:  a list of sources, with the index corresponding to the id
+    
+    steps int:    the maximum number of steps a single source can execute
+    step  func(): the function called after every single step has been executed
+                  for all sources
+    spr   int:    the granularity of each step
+    
+    callbacks {str:func(int, *args)}: a list of functions available to the
+                                      sources, the first argument passed into
+                                      the function is the source id
+    builtins  {str:func(*args)}:      a list of functions available to the sources
+    includes  str OR [str]:           a string or list of strings representing
+                                      jcli src that will be executed before the
+                                      main src is executed
+    """
     if builtins is None:
         builtins = jcli_builtins
+    if includes is None:
+        includes = open('jcli_includes.jcli').read()
+    if isinstance(includes, str):
+        includes = [includes]
+    for include in includes:
+        for step in eval_lisp(include, builtins): pass
     executors = []
     for index, src in enumerate(srcs):
         globals_ = dict(builtins)
@@ -68,3 +90,38 @@ def execute_lisp(bytecode, builtins=None, debug=False):
     if builtins is None:
         builtins = dict(jcli_builtins)
     return executor.execute_bytecodes(bytecode, builtins, debug)
+    
+if __name__ == '__main__':
+    import tokenizer
+    import parser_
+    import compiler
+    import assembler
+    import operator
+    import executor
+    
+    from builtins_ import jcli_builtins as builtins
+    try:
+        raw_input
+    except NameError:
+        raw_input = input
+    includes = open('jcli_includes.jcli').read()
+    if isinstance(includes, str):
+        includes = [includes]
+    for include in includes:
+        for step in eval_lisp(include, builtins): pass
+    globals_ = dict(builtins)
+    while True:
+        try:
+            src = raw_input('executor> ')
+            tokens = tokenizer.tokenize(src)
+            ast = parser_.parse(tokens)
+            asm = compiler.compile_(ast)
+            bc = assembler.assemble(asm)
+            ex = executor.execute_bytecodes(bc, globals_, debug=True)
+            while True:
+                try:
+                    next(ex)
+                except StopIteration as e:
+                    break
+        except Exception as e:
+            print(e.args[0])
